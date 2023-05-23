5b6c49fb1853737b6d3323fbc1e8d184fc084691
laza <lazadevop@gmail.com>
2023-05-23 20:33:47 +0200

    Mise en place Bdd

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,49 @@
+// Importation du module Express
 const express = require('express');
 
+// Création d'une instance de l'application Express
 const app = express();
 
+// Intercepter toutes les requetes Content-type au format json(pour post, get..)body -parser
+app.use(express.json());
 
+//Gerer les erreurs CORS
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+});
+
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({
+        message: 'Objet créé !'
+    });
+});
+
+app.get('/api/stuff', (req, res, next) => {
+    const stuff = [
+        {
+            _id: 'oeihfzeoi',
+            title: 'Mon premier objet',
+            description: 'Les infos de mon premier objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 4900,
+            userId: 'qsomihvqios',
+        },
+        {
+            _id: 'oeihfzeomoihi',
+            title: 'Mon deuxième objet',
+            description: 'Les infos de mon deuxième objet',
+            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+            price: 2900,
+            userId: 'qsomihvqios',
+        },
+    ];
+    res.status(200).json(stuff);
+});
+
+// Exportation de l'application pour une utilisation ultérieure
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,59 @@
+// Importation du module HTTP
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    res.end('Voilà ta reum !');
+// Importation du module d'application express
+const app = require('./app');
+
+// Fonction de normalisation du port d'écoute
+const normalizePort = val => {
+    const port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        return val;
+    }
+    if (port >= 0) {
+        return port;
+    }
+    return false;
+};
+
+// Normalisation du port d'écoute de l'application express
+const port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+// Gestionnaire d'erreurs du serveur HTTP
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
+
+// Création du serveur HTTP à partir de l'application express
+const server = http.createServer(app);
+
+// Définition du gestionnaire d'erreurs du serveur HTTP
+server.on('error', errorHandler);
+
+// Définition de l'action à exécuter lorsque le serveur écoute les requêtes entrantes
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
+    console.log('Listening on ' + bind);
 });
 
-server.listen(process.env.PORT || 3000); 
+// Lancement du serveur HTTP sur le port d'écoute
+server.listen(port);
